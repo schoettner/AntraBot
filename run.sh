25ab746da0c2a659a03bf0987c0e81536fae3345
bastian <bastian.schoettner@rewe-digital.com>
2019-03-24 19:38:58 +0100

    fix typo in travis deploy and remove prints from run script

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-echo "print env"
-echo $TWITCH_USERNAME
-echo $CLIENT_ID
-echo $TWITCH_TOKEN
-echo $CHANNEL
-
 echo "start bot"
 python3 ./antra_bot.py $TWITCH_USERNAME $CLIENT_ID $TWITCH_TOKEN $CHANNEL
